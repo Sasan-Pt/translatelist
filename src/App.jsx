c440bb859e2f84f73ab6c6fd9b2da17621856b6b
Ooze <sasan.pt.tec@gmail.com>
2025-05-08 18:09:10 +0330

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="grid grid-cols-[4%_92%_4%] h-full w-full  font-[family-name:var(--font-geist-sans)] ">
+      <div className="col-start-2 pt-4 leading-7 text-[16px] font-bold">
+        Translation Management
+      </div>
+    </div>
+  );
+}
+
+export default App;
